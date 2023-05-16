1e30f85c4dd0caf132621074e8dca55612c18c8d
MysteryPancake <MysteryPancake@users.noreply.github.com>
2023-05-16 20:30:11 +1000

    update button wip 2

--- blender_addon/__init__.py
+++ blender_addon/__init__.py
@@ -43,11 +43,8 @@ def tag_data(data, name: str, layer: str, version: int) -> None:
 		# Updated data, change version
 		data["sg_version"] = version
 
-# Usually artists stay in one layer (department specific)
-# Store layer as an addon pref so it stays between restarts
 class Preferences(bpy.types.AddonPreferences):
 	bl_idname = __name__
-	layer: bpy.props.IntProperty(name="Layer", default=0)
 	dev_mode: bpy.props.BoolProperty(name="Developer Mode", default=False)
 
 	def draw(self, context):
@@ -55,26 +52,18 @@ class Preferences(bpy.types.AddonPreferences):
 
 # Properties for this plugin shown in the UI
 class Properties(bpy.types.PropertyGroup):
-	def get_layer(self):
-		prefs = bpy.context.preferences
-		return prefs.addons[__name__].preferences.layer
-
-	def set_layer(self, value):
-		prefs = bpy.context.preferences
-		prefs.addons[__name__].preferences.layer = value
-
 	# Publish properties
-	layer: bpy.props.EnumProperty(name="Layer", items=layer_menu, get=get_layer, set=set_layer)
+	publish_layer: bpy.props.EnumProperty(name="Layer", items=layer_menu)
 	publish_asset: bpy.props.StringProperty(name="Asset Name")
 
 	# Fetch properties
 	fetch_asset: bpy.props.StringProperty(name="Asset Name")
 
 	# Developer properties
-	dev_make_folder: bpy.props.BoolProperty(name="(DEV) Make folder if missing")
+	dev_make_folder: bpy.props.BoolProperty(name="(DEV) Make asset if missing")
 	dev_build_asset: bpy.props.StringProperty(name="Asset Name")
 	dev_build_version: bpy.props.IntProperty(name="Layer Version", default=1)
-	dev_build_layer: bpy.props.EnumProperty(name="Build Layer", items=layer_menu)
+	dev_build_layer: bpy.props.EnumProperty(name="Layer", items=layer_menu)
 
 class Publish_Operator(bpy.types.Operator):
 	"""Save and increment the version of the above assets"""
@@ -85,7 +74,7 @@ class Publish_Operator(bpy.types.Operator):
 		blender_db = os.environ.get("SG_BLEND_DB")
 		props = context.scene.sg_props
 
-		if not props.layer:
+		if not props.publish_layer:
 			self.report({"ERROR_INVALID_INPUT"}, "Please select a layer!")
 			return {"CANCELLED"}
 		if not props.publish_asset:
@@ -97,11 +86,11 @@ class Publish_Operator(bpy.types.Operator):
 			if props.dev_make_folder:
 				os.makedirs(wip_folder)
 			else:
-				self.report({"ERROR"}, f"Folder for '{props.publish_asset}' doesn't exist yet! Add it on the website.")
+				self.report({"ERROR"}, f"Asset '{props.publish_asset}' doesn't exist yet! Add it on the website.")
 				return {"CANCELLED"}
 
 		# Structure is "master/wip/asset/layer/asset_layer_v001.blend" for now
-		layer_folder = os.path.join(wip_folder, props.layer)
+		layer_folder = os.path.join(wip_folder, props.publish_layer)
 		if not os.path.exists(layer_folder):
 			os.mkdir(layer_folder)
 
@@ -109,27 +98,27 @@ class Publish_Operator(bpy.types.Operator):
 		version = len([p for p in os.listdir(layer_folder) if p.endswith(".blend")]) + 1
 
 		# Name is "asset_layer_v001.blend" for now
-		file_name = f"{props.publish_asset}_{props.layer}_v{version:03d}.blend"
+		file_name = f"{props.publish_asset}_{props.publish_layer}_v{version:03d}.blend"
 		path = os.path.join(layer_folder, file_name)
 
 		# I'm using custom data to associate data blocks with an asset, version and layer
 		# First tag all collections and objects which haven't already been tagged
 		root = context.scene.collection
 		for col in root.children_recursive:
-			tag_data(col, props.publish_asset, props.layer, version)
+			tag_data(col, props.publish_asset, props.publish_layer, version)
 		for obj in root.all_objects:
-			tag_data(obj, props.publish_asset, props.layer, version)
+			tag_data(obj, props.publish_asset, props.publish_layer, version)
 
 		# Next tag sub-object data blocks
-		for data_type in layer_lookup[props.layer].trigger_update:
+		for data_type in layer_lookup[props.publish_layer].trigger_update:
 			for block in getattr(bpy.data, data_type):
-				tag_data(block, props.publish_asset, props.layer, version)
+				tag_data(block, props.publish_asset, props.publish_layer, version)
 
 		# Save a copy in the "wip" folder, this copy should never be touched!
 		bpy.ops.wm.save_as_mainfile(filepath=path, check_existing=True, copy=True)
 
 		# Would be nice to add a popup for this
-		success_msg = f"Published {props.publish_asset} {props.layer} version {version}!"
+		success_msg = f"Published {props.publish_asset} {props.publish_layer} version {version}!"
 		self.report({"INFO"}, success_msg)
 
 		return {"FINISHED"}
@@ -147,7 +136,7 @@ class Publish_Panel(bpy.types.Panel):
 
 		scn = context.scene
 		self.layout.prop(scn.sg_props, "publish_asset")
-		self.layout.prop(scn.sg_props, "layer")
+		self.layout.prop(scn.sg_props, "publish_layer")
 
 		# Developer-only option
 		if context.preferences.addons[__name__].preferences.dev_mode:
@@ -156,9 +145,10 @@ class Publish_Panel(bpy.types.Panel):
 		self.layout.operator(Publish_Operator.bl_idname, icon="EXPORT")
 
 def get_selected_names(context):
-	# Default to all visible objects if none are selected
-	selected = context.selected_objects
-	objects = selected if selected else context.visible_objects
+	objects = context.selected_objects
+	# Use visible objects when none are selected
+	if not objects:
+		objects = context.visible_objects
 	names = set()
 	for obj in objects:
 		name = obj.get("sg_asset")
@@ -176,25 +166,24 @@ class Check_Updates_Operator(bpy.types.Operator):
 		name_set = get_selected_names(context)
 
 		for layer in listed_layers:
-			for category in layer.trigger_update:
+			for data_type in layer.trigger_update:
+				for block in getattr(bpy.data, data_type):
+					asset = block.get("sg_asset")
+					# Blocks without sg_asset get skipped here too
+					if asset not in name_set:
+						continue
 
-				block = getattr(bpy.data, category)
-				name = block.get("sg_asset")
-				# Blocks without sg_asset get skipped here too
-				if name not in name_set:
-					continue
+					layer = block.get("sg_layer")
+					folder = os.path.join(blender_db, "wip", asset, layer)
+					if not os.path.exists(folder):
+						continue
 
-				layer = block.get("sg_layer")
-				folder = os.path.join(blender_db, "wip", asset, layer)
-				if not os.path.exists(folder):
-					continue
+					current = block.get("sg_version")
+					latest = len([p for p in os.listdir(folder) if p.endswith(".blend")])
 
-				current = block.get("sg_version")
-				latest = len([p for p in os.listdir(folder) if p.endswith(".blend")]) + 1
-
-				print(f"Current: {current}, Latest: {latest}")
-				if latest > current:
-					print("OUT OF DATE")
+					print(f"Current: {current}, Latest: {latest}")
+					if latest > current:
+						print("OUT OF DATE")
 
 		return {"FINISHED"}
 
@@ -208,7 +197,7 @@ class Update_Panel(bpy.types.Panel):
 	def draw(self, context):
 		name_set = get_selected_names(context)
 		names = ", ".join(name_set) if name_set else "None found"
-		label = names if selected else f"All ({names})"
+		label = names if context.selected_objects else f"All ({names})"
 
 		self.layout.label(text=f"Selected: {label}")
 		self.layout.operator(Check_Updates_Operator.bl_idname, icon="FILE_REFRESH")
